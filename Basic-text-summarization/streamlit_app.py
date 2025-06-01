5f10fd1cc383a29163ad4968fa2768bd61553f27
Bhavya-Mistry <mistrybhavya9@gmail.com>
2025-06-01 14:51:17 +0530

    Update streamlit_app.py

--- Basic-text-summarization/streamlit_app.py
+++ Basic-text-summarization/streamlit_app.py
@@ -10,20 +10,24 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 # Summarization function
+
 def summarize_text(article_text):
     article_text = article_text.lower()
     clean_text = re.sub(r'[^a-zA-Z]', ' ', article_text)
     clean_text = re.sub(r'\s+', ' ', clean_text)
 
-    # Use explicit tokenizer to avoid punkt_tab bug
+    # Use direct Punkt tokenizer to avoid NLTK bugs
     from nltk.tokenize import PunktSentenceTokenizer
     tokenizer = PunktSentenceTokenizer()
     sentence_list = tokenizer.tokenize(article_text)
 
-    stop_words = set(stopwords.words('english'))
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+
+    # Replace word_tokenize with regex-based split
+    words = re.findall(r'\b\w+\b', clean_text)
 
     word_frequencies = {}
-    for word in word_tokenize(clean_text):
+    for word in words:
         if word not in stop_words:
             word_frequencies[word] = word_frequencies.get(word, 0) + 1
 
@@ -33,12 +37,14 @@ def summarize_text(article_text):
     sentence_scores = {}
     for sentence in sentence_list:
         if len(sentence.split()) < 30:
-            for word in word_tokenize(sentence.lower()):
+            sentence_words = re.findall(r'\b\w+\b', sentence.lower())
+            for word in sentence_words:
                 if word in word_frequencies:
                     sentence_scores[sentence] = sentence_scores.get(sentence, 0) + word_frequencies[word]
 
     summary_sentences = heapq.nlargest(5, sentence_scores, key=sentence_scores.get)
     return " ".join(summary_sentences)
+
 
 # Streamlit UI
 st.title("📝 Basic Text Summarizer")
